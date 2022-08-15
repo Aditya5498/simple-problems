5158a3fd3544ace1db51b39c0d8480f708ecfad3
unknown <adi2tya98@gmail.com>
2022-08-15 11:19:35 +0530

    reverse K nodes from linked list amd reverse alternate nodes from linked list

--- PracticeLinkedList/LinkedList.cpp
+++ PracticeLinkedList/LinkedList.cpp
@@ -87,6 +87,84 @@ present->next=prev;
 return present;
 }
 
+// Reverses the linked list in groups
+// of size k and returns the pointer
+// to the new head node.
+//function to reverse every k nodes -> https://www.geeksforgeeks.org/reverse-a-list-in-groups-of-given-size/
+
+Node* reverseKnodes(Node *head,int k){
+// base case
+    if (!head)
+        return NULL;
+    Node* current = head;
+    Node* next = NULL;
+    Node* prev = NULL;
+    int count = 0;
+  
+    /*reverse first k nodes of the linked list */
+    while (current != NULL && count < k) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+        count++;
+    }
+  
+    /* next is now a pointer to (k+1)th node
+    Recursively call for the list starting from current.
+    And make rest of the list as next of first node */
+    if (next != NULL)
+        head->next = reverseKnodes(next, k);
+  
+    /* prev is new head of the input list */
+    return prev;
+}
+
+/* Reverses alternate k nodes and 
+returns the pointer to the new head node */
+Node *kAltReverse(Node *head, int k) 
+{ 
+    Node* current = head; 
+    Node* next; 
+    Node* prev = NULL; 
+    int count = 0; 
+  
+    /*1) reverse first k nodes of the linked list */
+    while (current != NULL && count < k) 
+    { 
+    next = current->next; 
+    current->next = prev; 
+    prev = current; 
+    current = next; 
+    count++; 
+    } 
+      
+    /* 2) Now head points to the kth node. 
+    So change next  of head to (k+1)th node*/
+    if(head != NULL) 
+    head->next = current; 
+  
+    /* 3) We do not want to reverse next k 
+       nodes. So move the current 
+        pointer to skip next k nodes */
+    count = 0; 
+    while(count < k-1 && current != NULL ) 
+    { 
+    current = current->next; 
+    count++; 
+    } 
+  
+    /* 4) Recursively call for the list 
+    starting from current->next. And make
+    rest of the list as next of first node */
+    if(current != NULL) 
+    current->next = kAltReverse(current->next, k); 
+  
+    /* 5) prev is new head of the input list */
+    return prev; 
+} 
+  
+
 int main(){
     Node *head=new Node(1);
     head->next=new Node(2);
@@ -104,6 +182,9 @@ int main(){
     findMiddle(head);
     printList(head);
     head=reverseLinkedList(head);
+    printList(head);
+    cout<<"reverse alternate every 3 nodes of above Linked list\n";
+    head=kAltReverse(head,3);
     printList(head);
     return 0;
 }
