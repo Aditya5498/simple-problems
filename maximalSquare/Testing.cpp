485484aa3da1817568a6f76b380b43e2a4a24819
unknown <adi2tya98@gmail.com>
2022-08-24 12:01:09 +0530

    Completed the program of maximal Square -> Leetcode problem

--- maximalSquare/Testing.cpp
+++ maximalSquare/Testing.cpp
@@ -1,3 +1,4 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <vector>
 #include <climits>
@@ -28,6 +29,45 @@ void print(T &arr){
         cout<<endl;
     }
 }
+
+void DPapproach(vector<vector<char>> &arr,vector<vector<int>> &dp){
+       int Erow=arr.size(),Scol=arr[0].size();
+while(Erow>0 && Scol>0){
+    for(int i=0;i<Erow;i++){
+      //  cout<<arr[i][Scol-1]<<" ";
+        if(arr[i][Scol-1]=='1'){
+            dp[i][Scol-1] = min(dp[i][Scol], min(dp[i+1][Scol-1],dp[i+1][Scol])) +1;
+        }
+        else{
+            dp[i][Scol-1]=0;
+        }
+    }
+    Scol--;
+    for(int i=Scol-1;i>=0;i--){
+      //  cout<<arr[Erow-1][i]<<" ";
+        if(arr[Erow-1][i]=='1'){
+            dp[Erow-1][i] = min(dp[Erow-1][i+1], min(dp[Erow][i],dp[Erow][i+1])) +1;
+        }
+        else{
+        dp[Erow-1][i]=0;
+        }
+    }
+    cout<<endl;
+    Erow--;
+}
+}
+
+//Find max in a 2d array
+int findMax(vector<vector<int>> &arr){
+    int max=INT_MIN;
+    for(int i=0;i<arr.size();i++){
+        for(int j=0;j<arr[0].size();j++){
+            max=arr[i][j]>max?arr[i][j]:max;
+        }
+    }
+    return max;
+}
+
 int main() {
     // Write C++ code here
    vector<vector<char>> arr={
@@ -41,10 +81,21 @@ print(arr);
 vector<vector<int>> maximalSquare(arr.size()+1, vector<int> (arr[0].size()+1));
 
 // add 0 in all cells of last row and column
-for(int i=0;i<arr.size();i++) maximalSquare[i][arr[0].size()-1]=0;
+for(int i=0;i<arr.size();i++) maximalSquare[i][arr[0].size()]=0;
+for (int i=0;i<arr[0].size();i++) maximalSquare[arr.size()][i]=0;
+maximalSquare[arr.size()][arr[0].size()]=0;
 
-printL(maximalSquare);
-print(maximalSquare);
+// compare with each cell in array and with right cell, 
+//bottom cell and diagonal cell. 
+//Add 1 if the present cell in array has '1' else keep it '0'
+DPapproach(arr,maximalSquare);
+
+// find maximum int in maximalSquare 2d matrix which is the side of the square
+int max=findMax(maximalSquare);
+cout<<"Maximimum side of the square = "<<max<<" \n=> Area of the square = "<<max*max<<endl;
+
+// printL(maximalSquare);
+// print(maximalSquare);
 
     return 0;
 }
