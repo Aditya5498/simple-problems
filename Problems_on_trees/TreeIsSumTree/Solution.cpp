e8576dac6d796ab3589a2635750d9a059cb09025
unknown <adi2tya98@gmail.com>
2022-09-14 20:25:22 +0530

    Recursive solution to check if a tree is sum tree

--- Problems_on_trees/TreeIsSumTree/Solution.cpp
+++ Problems_on_trees/TreeIsSumTree/Solution.cpp
@@ -37,6 +37,46 @@ bool isTreeSum(Node *root){
     return false;
 }
 
+// A utility function to get the sum
+// of values in tree with root as root
+int sum(Node *root)
+{
+    if (root == NULL)
+        return 0;
+     
+    return sum(root->left) + root->data +
+           sum(root->right);
+}
+ 
+
+// Returns 1 if sum property holds for
+// the given node and both of its children
+int isSumTree(Node* node)
+{
+    int ls, rs;
+ 
+    // If node is NULL or it's a leaf
+    // node then return true
+    if (node == NULL ||
+       (node->left == NULL &&
+        node->right == NULL))
+        return 1;
+ 
+   // Get sum of nodes in left and
+   // right subtrees
+   ls = sum(node->left);
+   rs = sum(node->right);
+ 
+   // If the node and both of its
+   // children satisfy the property
+   // return 1 else 0
+    if ((node->data == ls + rs) &&
+          isSumTree(node->left) &&
+          isSumTree(node->right))
+        return 1;
+ 
+   return 0;
+}
 
 int main(){
     Node *root = new Node(26);
@@ -46,6 +86,6 @@ int main(){
     root->left->right = new Node(6);
     root->right->right = new Node(3);
 
-    cout<<" Check if the Tree is Sum Tree\n"<<isTreeSum(root)<<endl;
+    cout<<" Check if the Tree is Sum Tree\n"<<isSumTree(root)<<endl;
     return 0;
 }
