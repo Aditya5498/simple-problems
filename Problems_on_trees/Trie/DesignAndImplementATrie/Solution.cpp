cbaa86ab3b9253efc8cabb620fa5170c14f60cce
unknown <adi2tya98@gmail.com>
2022-09-14 19:50:49 +0530

    added a simple function to print onle 1 word

--- Problems_on_trees/Trie/DesignAndImplementATrie/Solution.cpp
+++ Problems_on_trees/Trie/DesignAndImplementATrie/Solution.cpp
@@ -7,19 +7,41 @@ TrieNode *child[26];
 bool isEndOfWord;
 
 TrieNode(){
-    for(int i=0;i<26;i++) child[i] = NULL;
+    for(int i=0;i<26;i++) child[i] = nullptr;
     isEndOfWord = false;
 }
 };
 
 void insert(TrieNode *root, string key){
     for(int i=0;i<key.size();i++){
-        if(root->child[key[i] - 'a'] == NULL)  root -> child[key[i] - 'a'] = new TrieNode();
+        if(root->child[key[i] - 'a'] == NULL)  {
+            cout<<"Inserting "<<key[i]<<" at\t"<<key[i]-'a'<<endl;
+            root -> child[key[i] - 'a'] = new TrieNode();
+        }
          root = root->child[key[i] - 'a'];
     }
     root -> isEndOfWord = true;
 }
 
+void printWords(TrieNode *root){
+    while(root){
+    for(int i=0;i<26;i++){
+        if(root->child[i]){
+            char c = 'a' + i;
+            cout<<c<<" ";
+    root = root ->child[i];
+        }
+    }
+    if(root->isEndOfWord) break;
+    }
+}
+
 int main(){
+    TrieNode *root = new TrieNode();
+    string t = "teat";
+    insert(root,t);
+    t = "marks";
+    insert(root, t);
+    printWords(root);
     return 0;
 }
