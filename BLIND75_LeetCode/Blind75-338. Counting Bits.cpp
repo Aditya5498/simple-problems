bdfb547489dc2c61dd35ccd2f72325027e9b8b46
unknown <adi2tya98@gmail.com>
2022-10-10 08:09:49 +0530

    Added 2 new problems of Blind 75

---
+++ BLIND75_LeetCode/Blind75-338. Counting Bits.cpp
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/counting-bits/description/
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n+1,0);
+        dp[0] = 0;
+        int offset = 1;
+
+        for(int i=1;i<n+1;i++){
+            if(offset*2 == i) offset = i;
+            dp[i] = 1 + dp[i-offset];
+        }
+        return dp;
+    }
+};
