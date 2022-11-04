1c8a0f7ddd1f3e197e736e38e1694206b54273e2
unknown <adi2tya98@gmail.com>
2022-11-04 09:17:09 +0530

    Infix to Postfix conversion

---
+++ DSLab/InfixToPostfix.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+
+using namespace std;
+
+int getPrecedence(char chr){
+    if(chr == '/' || chr == '*') return 2;
+    return 1;
+}
+
+void popAll(stack<char> &st){
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+}
+
+void convertInfixToPostfix(string str){
+    stack<char> st;
+    for(int i=0;i<str.length();i++){
+        if(str[i] >= 'a' && str[i] <= 'z') cout<<str[i]<<" ";
+        else if(st.empty()) st.push(str[i]);
+        else if(getPrecedence(st.top()) >= getPrecedence(str[i])){
+            popAll(st);
+            st.push(str[i]);
+        }        
+        else st.push(str[i]);
+    }
+    popAll(st);
+    cout<<endl;
+}
+
+int main(){
+    string str = "a+b*c";
+    cout<<"Conversion of "<<str<<" to postfix expression is\n";
+    convertInfixToPostfix(str);
+    str = "a*b+c";
+    cout<<"Conversion of "<<str<<" to postfix expression is\n";
+    convertInfixToPostfix(str);
+    return 0;
+}
